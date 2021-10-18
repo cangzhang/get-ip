35916d0b041e65e6ecb68d703a2a34c4f3d9d6e3
al <cangzan@gmail.com>
2021-10-18 21:42:32 +0800

    feat: get ip.

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
 
 func main() {
-	fmt.Println("127.0.0.1")
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "https://2021.ip138.com/", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	html := string(body)
+	r := regexp.MustCompile("\\d+.\\d+.\\d+.\\d+")
+	ip := r.FindString(html)
+	fmt.Println(ip)
 }
 
